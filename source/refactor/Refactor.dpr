dade34c200449e575496056c3693e7116a2d6eed
Krid <webmaster@dirklippert.de>
2023-05-11 19:12:51 +0200

    Initial Setup

---
+++ source/refactor/Refactor.dpr
@@ -0,0 +1,14 @@
+program Refactor;
+
+uses
+  Vcl.Forms,
+  Scanner in 'Scanner.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
